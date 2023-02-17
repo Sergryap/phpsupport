7102b60f05ce81470c26e02a312b315537ab10b6
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-02-17 22:46:46 +0200

    Add manager page

--- phpsupport/settings.py
+++ phpsupport/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 
 env = Env()
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'phpsupport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,8 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- service/views.py
+++ service/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Order
 
-# Create your views here.
+
+@login_required(login_url='users:sign-in')
+def view_index(request):
+    orders = Order.objects.all()
+    processed_orders = []
+    for order in orders:
+        order_attr_for_managers = {
+            'id': order.id,
+            'title': order.title,
+            'customer': order.client,
+            'freelancer': order.freelancer,
+            'status': order.get_status_display(),
+            'created_at': order.created_at,
+            'deadline': order.deadline
+        }  
+        processed_orders.append(order_attr_for_managers)
+
+    return render(request, 'service/index.html', context={
+        'processed_orders': processed_orders
+    })
