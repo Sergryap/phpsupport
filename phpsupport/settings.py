e079ebf009050cf808799f5c0e3312227029decb
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-02-18 20:59:01 +0200

    Update settings.py

--- phpsupport/settings.py
+++ phpsupport/settings.py
@@ -32,7 +32,7 @@ TELEGRAM_TOKEN=env.str('TELEGRAM_TOKEN')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://web-production-8c3f.up.railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-3e29.up.railway.app', '127.0.0.1']
 
 
 # Application definition
